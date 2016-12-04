5e6db932fb6fffba8313eb4fb40234877fb1ece9
isengardd <xuyao8598@msn.com>
2016-12-05 00:00:25 +0800

    增加从DB获取历史数据的逻辑，增加配置文件

--- StockPool.py
+++ StockPool.py
@@ -1,17 +1,36 @@
 #encoding=utf-8
 __author__ = 'yao xu'
 
-class StockHistory:
-    def __init__(self, stock_id, price_high, price_N, price_recent_avg):
+import pymysql
+import os
+import json
+
+class StockData:
+    def __init__(self, stock_id, price_high, price_after_buy, price_N, price_recent_avg):
         self.id = stock_id
         self.price_high = price_high
         self.price_N = price_N
         self.price_recent_avg = price_recent_avg
-
+        self.price_after_buy = price_after_buy
 class StockPool:
     def __init__(self):
         self.m_stock_pool = dict()
+        self.config_path = os.path.dirname(__file__) + '/trader_config.json'
+        with open(self.config_path, encoding='utf-8') as f:
+             self.config = json.load(f)
 
     def load_stock_pool(self):
         #读取配置信息
+        conn = pymysql.connect(host=self.config['dbhost'], user=self.config['dbuser'], passwd=self.config['passwd'],
+                               db=self.config['dbname'], port=int(self.config['dbport']), charset="utf8")
+        cursor=conn.cursor()
+
+        cursor.execute("SELECT stock_id,price_high,price_high_after_buy,price_N,price_recent_avg FROM stock_pool")
+        rows = cursor.fetchall()
+        for row in rows:
+            self.m_stock_pool[row[0]] = StockData(row[0], float(row[1]), float(row[2]), float(row[3]), float(row[4]))
+            #print(self.m_stock_pool[row[0]].__dict__ )
+
+        cursor.close()
+        conn.close()
         pass
--- Strategy.py
+++ Strategy.py
@@ -2,13 +2,19 @@
 __author__ = 'yao xu'
 
 import easyquotation
+import easytrader
 import time
 import datetime
 import StockPool
+import os
 
 class Strategy:
     def __init__(self):
         self.m_stocks = StockPool.StockPool()
+        self.m_quotation = easyquotation.use('sina')
+        self.m_trader = easytrader.use('yjb', False)
+        config_path = os.path.dirname(__file__) + '/trader_config.json'
+        self.m_trader.prepare(config_path)
 
     def run(self):
         while True:
@@ -37,14 +43,34 @@ class Strategy:
         # 加载远端数据
         self.load_stock_pool()
         # 读取账户资金信息，包括现金和仓位
+        my_balance = self.m_trader.balance
+        print(my_balance)
+        my_position = self.m_trader.position
+        print(my_position)
+
         #获取每只股票的当前价格
-        #决定是否交易
+        for idx in self.m_stocks.m_stock_pool.keys():
+            stock_data = self.m_stocks.m_stock_pool[idx]
+            print(stock_data.id)
+            result = self.m_quotation.stocks(stock_data.id)
+            if result is None:
+                easytrader.log.info("request %s stock info failed!" %stock_data.id)
+            else :
+                print(result)
+            #决定是否交易
+            self.decide_sell()
+            self.decide_buy()
 
         #从委托列表中执行相应的命令
 
 
     def load_stock_pool(self):
-        self.m_stocksk.load_stock_pool()
+        self.m_stocks.load_stock_pool()
 
+    def decide_sell(self):
+        pass
+
+    def decide_buy(self):
+        pass
 
 
